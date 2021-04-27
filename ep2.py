52489d3e12a497620390edeed88379ec8391dc6c
luanams <lsorpreso@uol.com.br>
2021-04-27 17:23:38 -0300

    commit 10

--- ep2.py
+++ ep2.py
@@ -28,7 +28,3 @@ def lista_movimentos_possiveis(cartas,indice):
             if listavalores[indice] == listavalores[indice-3] or listanaipe[indice] == listanaipe[indice-3]:
                 resultado.append(3)
     return resultado
-            
-
-
-print(lista_movimentos_possiveis(['6♥', 'J♥', '9♣', '9♥'],1))
