a072d68506e2c0e9cc51c1f66313049f4fa557ce
luanams <lsorpreso@uol.com.br>
2021-04-27 14:06:32 -0300

    commit 3

--- ep2.py
+++ ep2.py
@@ -5,3 +5,37 @@ def cria_baralho():
 def extrai_valor(carta):
     valor = carta [0:len(carta)-1]
     return valor
+    
+def extrai_naipe(carta):
+    naipe = carta[len(carta)-1]
+    return naipe
+
+def lista_movimentos_possiveis(baralho,indice):
+    movimentos = 0 
+    for i in range(0,len(baralho)-1):
+        naipe = extrai_naipe(baralho[i])
+        valor = extrai_valor(baralho[i])
+        if indice == '0':
+            resultado = []
+        if indice == '1':
+            if valor[1] == valor[0] or naipe[1] == naipe[0]:
+                movimentos += 1
+        if indice == '2':
+            if valor[2] == valor[1] or naipe[2] == naipe[1]:
+                movimentos += 1
+            if valor[2] == valor[0] or naipe[2] == naipe[0]:
+                movimentos += 3
+        if indice == '3':
+            if valor[3] == valor[2] or naipe[3] == naipe[2]:
+                movimentos += 1
+            if valor[3] == valor[1] or naipe[3] == naipe[1]:
+                movimentos += 3
+    if movimentos == '4':
+        resultado = [1,3]
+    if movimentos == '1':
+        resultado = [1]
+    if movimentos == '3':
+        resultado = [3]
+    return resultado
+
+print(lista_movimentos_possiveis(['6♥', 'J♥', '9♣', '9♥'],1))
