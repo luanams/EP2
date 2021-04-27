ed37ee1c5b74c8526f30c10d090642d0f3f8ee8c
luanams <lsorpreso@uol.com.br>
2021-04-27 14:42:01 -0300

    commit 6

--- ep2.py
+++ ep2.py
@@ -11,30 +11,26 @@ def extrai_naipe(carta):
     return naipe
 
 def lista_movimentos_possiveis(baralho,indice):
-    movimentos = 0 
-    resultado = []
     for i in range(0,len(baralho)):
         naipee = extrai_naipe(baralho[i])
         valorr = extrai_valor(baralho[i])
-        if indice == 1:
+        if indice == '1':
             if valorr[1] == valorr[0] or naipee[1] == naipee[0]:
-                movimentos += 1
-        if indice == 2:
-            if valorr[2] == valorr[1] or naipee[2] == naipee[1]:
-                movimentos += 1
+                return([1])
+        if indice == '2':
+            if valorr[2] == valorr[1] or naipee[2] == naipee[1]: 
+                return([1])
             if valorr[2] == valorr[0] or naipee[2] == naipee[0]:
-                movimentos += 3
-        if indice == 3:
+                return([3])
+            if valorr[2] == valorr[1] or naipee[2] == naipee[1] and valorr[2] == valorr[0] or naipee[2] == naipee[0]:
+                return([1,3])
+        if indice == '3':
             if valorr[3] == valorr[2] or naipee[3] == naipee[2]:
-                movimentos += 1
+                return([1])
             if valorr[3] == valorr[1] or naipee[3] == naipee[1]:
-                movimentos += 3
-    if movimentos == 4:
-        resultado.append('1','3')
-    if movimentos == 1:
-        resultado.append('1')
-    if movimentos == 3:
-        resultado.append('3')
-    return resultado
+                return([3]) 
+            if valorr[3] == valorr[2] or naipee[3] == naipee[2] and valorr[3] == valorr[1] or naipee[3] == naipee[1]:
+                return([1,3])
+
 
 print(lista_movimentos_possiveis(['6♥', 'J♥', '9♣', '9♥'],1))
