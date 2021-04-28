31efa49298f1271893f251e1697e3bf2b932a9a2
luanams <lsorpreso@uol.com.br>
2021-04-28 13:55:51 -0300

    commit 14

--- ep2.py
+++ ep2.py
@@ -37,7 +37,7 @@ def empilha(baralho,origem,destino):
 def possui_movimentos_possiveis(baralho):
     for i in range(0,len(baralho)-1):
         movimentos = lista_movimentos_possiveis(baralho,i)
-        if movimentos == [1,3] or movimentos == [1] or movimentos == [3]:
-            return True
-        else:
-            return False
+    if movimentos == [1,3] or movimentos == [1] or movimentos == [3]:
+        return True
+    else:
+        return False
