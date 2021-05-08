f5fb7c7fad6c7c5d227ef814a9210392af7b7144
luanams <lsorpreso@uol.com.br>
2021-05-08 15:46:45 -0300

    commit 1

--- ep2 final.py
+++ ep2 final.py
@@ -2,4 +2,8 @@ import random
 baralho = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣'] 
 
 random.shuffle(baralho)
-
+dicionario = {}
+for i in range(0,len(baralho) - 1) :
+    ordem = baralho[i]
+    dicionario[i] = ordem
+print(dicionario)
