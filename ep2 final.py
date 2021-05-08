b1f27025a1ca9410ab425aff072b885fdc246f17
luanams <lsorpreso@uol.com.br>
2021-05-08 20:58:53 -0300

    commit 4

--- ep2 final.py
+++ ep2 final.py
@@ -16,6 +16,26 @@ while jogo:
     def extrai_naipe(carta):
         naipe = carta[len(carta)-1]
         return naipe
-    jogo = False
-print(extrai_naipe(carta))
-print(extrai_valor(carta))
+    def lista_movimentos_possiveis(cartas,pergunta):
+        if pergunta == 1 or pergunta == 2:
+            cartas = [baralho[pergunta],baralho[pergunta-1]]
+        if pergunta > 2:
+            cartas = [baralho[pergunta],baralho[pergunta-1],baralho[pergunta-3]]
+        listanaipe = []
+        listavalores = []
+        resultado = []
+        for i in range(0,len(cartas)):
+            naipes = extrai_naipe(cartas[i])
+            valores = extrai_valor(cartas[i])
+            listanaipe.append(naipes)
+            listavalores.append(valores)
+        if pergunta == 0:
+            jogo == False
+        else:
+            if listavalores[pergunta] == listavalores[pergunta-1] or listanaipe[pergunta] == listanaipe[pergunta-1]:
+                resultado.append(1)
+            else:
+            if pergunta > 2:  
+                if listavalores[pergunta] == listavalores[pergunta-3] or listanaipe[pergunta] == listanaipe[pergunta-3]:
+                    resultado.append(3)
+        return resultado
