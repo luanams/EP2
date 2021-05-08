3d11d50d0f9e3cd5cb5dd4a86c5c2e7746c2ab53
luanams <lsorpreso@uol.com.br>
2021-05-08 20:21:35 -0300

    commit 3

--- ep2 final.py
+++ ep2 final.py
@@ -4,4 +4,18 @@ baralho = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠
 random.shuffle(baralho)
 
 for i in range(0,52) :
-    print('\n'+str(i)+ ': '+baralho[i])
+    print('\n'+str(i)+ '. '+baralho[i])
+jogo = True
+
+while jogo:
+    pergunta = int(input('escolha uma carta de 0 a 51: '))
+    carta = baralho[pergunta]
+    def extrai_valor(carta):
+        valor = carta [0:len(carta)-1]
+        return valor
+    def extrai_naipe(carta):
+        naipe = carta[len(carta)-1]
+        return naipe
+    jogo = False
+print(extrai_naipe(carta))
+print(extrai_valor(carta))
