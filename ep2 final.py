aadf3035d1c596cbb4df88c847839d3ed1855cc8
luanams <lsorpreso@uol.com.br>
2021-05-11 10:10:55 -0300

    commit

--- ep2 final.py
+++ ep2 final.py
@@ -2,40 +2,70 @@ import random
 baralho = ['A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣'] 
 
 random.shuffle(baralho)
-
-for i in range(0,52) :
-    print('\n'+str(i)+ '. '+baralho[i])
 jogo = True
 
+def extrai_valor(carta):
+    valor = carta [0:len(carta)-1]
+    return valor
+def extrai_naipe(carta):
+    naipe = carta[len(carta)-1]
+    return naipe
+
 while jogo:
-    pergunta = int(input('escolha uma carta de 0 a 51: '))
+    for i in range(0,len(baralho)) :
+        print('\n'+str(i)+ '. '+baralho[i])
+    pergunta = int(input('escolha uma carta: '))
     carta = baralho[pergunta]
-    def extrai_valor(carta):
-        valor = carta [0:len(carta)-1]
-        return valor
-    def extrai_naipe(carta):
-        naipe = carta[len(carta)-1]
-        return naipe
     def lista_movimentos_possiveis(cartas,pergunta):
-        if pergunta == 1 or pergunta == 2:
-            cartas = [baralho[pergunta],baralho[pergunta-1]]
-        if pergunta > 2:
-            cartas = [baralho[pergunta],baralho[pergunta-1],baralho[pergunta-3]]
+        cartas = []
+        if pergunta == '1' or pergunta == '2':
+            cartas.append(carta - 1) 
+            cartas.append(carta)
+        if pergunta > '2':
+            cartas.append(carta - 3)
+            cartas.append(carta - 2)
+            cartas.append(carta - 1)
+            cartas.append(carta)
         listanaipe = []
         listavalores = []
         resultado = []
-        for i in range(0,len(cartas)):
-            naipes = extrai_naipe(cartas[i])
-            valores = extrai_valor(cartas[i])
+        
+        for a in range(0,len(cartas)):
+            naipes = extrai_naipe(cartas[a])
+            valores = extrai_valor(cartas[a])
             listanaipe.append(naipes)
             listavalores.append(valores)
-        if pergunta == 0:
-            jogo == False
-        else:
+            
+        if pergunta != 0:
             if listavalores[pergunta] == listavalores[pergunta-1] or listanaipe[pergunta] == listanaipe[pergunta-1]:
                 resultado.append(1)
-            else:
-            if pergunta > 2:  
-                if listavalores[pergunta] == listavalores[pergunta-3] or listanaipe[pergunta] == listanaipe[pergunta-3]:
-                    resultado.append(3)
+        if pergunta > 2:  
+            if listavalores[pergunta] == listavalores[pergunta-3] or listanaipe[pergunta] == listanaipe[pergunta-3]:
+                resultado.append(3)
+        if resultado == [1,3]:
+            print('\n'+str(i)+ '. '+(carta - 3)+(carta-1) )
+            opcao = int(input('Para qual das duas gostaria de movimentar?' ))
         return resultado
+
+        def empilha(baralho,pergunta,destino):
+            if resultado == [1]:
+                destino = cartas[carta-1]
+            if resultado == [1,3]:
+                destino = cartas[opcao]
+            baralho[destino] = baralho[pergunta]
+            del(baralho[pergunta])
+            return baralho
+
+    def possui_movimentos_possiveis(baralho):
+        sim = 0
+        for i in range(0,len(baralho)-1):
+            movimentos = lista_movimentos_possiveis(baralho,i)
+            if movimentos == []:
+                sim += 0
+                jogo = False
+            else:
+                sim += 1
+        if sim != 0:
+            return True
+        else:
+            return False
